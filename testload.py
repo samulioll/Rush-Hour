fed6da18e41b64c34ed8aa35b04c99faa8623657
samulioll <120379431+samulioll@users.noreply.github.com>
2023-01-10 10:50:30 +0200

    Delete testload.py

--- testload.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("levels.json", "r+") as document:
-    all_levels = json.load(document)
-    print(all_levels)
-    for level in all_levels:
-        print(f"Name: {level[0]}")
-        for line in level[1]:
-            print(line)
